078cdd0b1505aebafb15288a3fc036aa43abf4b0
Henry Bigelow <hrbigelow@gmail.com>
2024-02-11 11:12:06 -0800

    split out kernel pseudocode docs

--- kernel_pseudocode.py
+++ kernel_pseudocode.py
@@ -3,78 +3,7 @@ This is Python pseudo-code to represent the structure of
 selective_scan_fwd_kernel from 
 csrc/selective_scan/selective_scan_fwd_kernel.cuh, line 69
 
-References to code there are commit 6dbfc455
-given as K + line number
-in csrc/selective_scan/selective_scan_fwd_kernel.cuh
-
-The overall structure uses a 2D grid (batch, dim) and a 1D thread block of size
-nthreads such that nthreads * nitems = chunk.  A chunk is a fixed-size contiguous
-range of the input sequence.
-
-`dim` refers to D, the number of input channels.  Note that in CUDA, there must be no
-assumption about what order each thread block runs.  This implies that the channels
-are supposed to be independent of each other.
-
-Within one block, each thread works in a nested loop.  The outer loop goes over
-chunks of size 2048 timesteps of the sequence.  The inner loop goes over the elements
-of the hidden state.
-
-Within a given chunk, a given thread processes only nitems of consecutive timesteps
-of that chunk.
-
-At K333 one can see that the values for nthreads, nitems are:
-
-nthreads    nitems    seqlen
-32          4         [0, 128] 
-32          8         (128, 256]
-32          16        (256, 512]
-64          16        (512, 1024]
-128         16        (1024, inf]
-
-Thus thread thread_id will process items 
-[thread_id * nitems, (thread_id + 1) * nitems)
-
-In the python code below, the block / threads hierarchy is represented at the top
-level
-
-Ordinarily, one thread block handles one (batch, dim) element of the input.  However,
-there is another constant kNRows, ordinarily set to 1, but which serves as a
-companion dimension to dim.   See K307.  If kNRows were set larger than 1, then each
-input would need to expand the [dim] sub-shape to [dim, kNRows].  There doesn't seem
-to be any plan to use kNRows other than 1, and if I'm not mistaken, it would actually
-break the existing code because in selective_scan.cpp, the CHECK_SHAPE calls do not
-allow for that extra dimension. 
-
-Then, there is the n_groups variable.  It is assumed to be 1 if you provide B and C
-with shapes [batch, dstate, seqlen].  You can also provide B and C with shape [batch,
-n_groups, dstate, seqlen] however.  This has the effect that it groups the dim
-dimension into n_groups groups.
-
-How does this work?  In the default case, we have:
-
-Bbar_{bdln} = Delta_{bld} B_{bnl}
-y_{bdl} = h_{bdnl} C_{bnl}
-
-In the case where n_groups > 1, we have instead
-
-Bbar_{bdln} = Delta_{bld} B'_{bdnl}
-y_{bdl} = h_{bdnl} C'_{bdnl}
-
-where:
-B'_{bdnl} = B_{bgnl} 
-C'_{bdnl} = C_{bgnl} 
-
-for g = d // dims_per_group
-
-
-Use of shared memory within a thread block
-
-There seems to be very minimal shared memory use within a thread block.  The
-InclusiveScan uses smem_running_prefix to communicate the partial results between
-threads, since each thread processes a different sub-range of the input sequence.
-Besides that, all shared memory use is wrapped in the load* statements, which use
-cub::BlockLoad.  It's not clear how this actually shares memory between threads - it
-doesn't appear that threads actually read data that another thread writes.
+Please see kernel_pseudocode.md for detail 
 """
 
 def run_kernel(nthreads, nitems, u, delta, A, B, C, out):
